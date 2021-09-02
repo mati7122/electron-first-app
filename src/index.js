4798b3eb9c6cfcecb28c87b4a0157c4d99ef670a
Matías Herrera <matias7122@gmail.com>
2021-09-01 22:20:06 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,98 @@
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+
+const url = require('url');
+const path = require('path');
+
+if(process.env.NODE_ENV !== 'production'){
+    require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, '../node_modules','.bin','electron')
+    })
+}
+
+let mainWindow;
+let newProductWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'view/index.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+
+    const mainMenu = Menu.buildFromTemplate(templateMenu);
+    Menu.setApplicationMenu(mainMenu);
+
+    mainWindow.on('closed', () => {
+        app.quit();
+    })
+});
+
+function createNewProductWindow() {
+    newProductWindow = new BrowserWindow({
+        width: 400,
+        height: 330,
+        title: 'Add a new product'
+    });
+    // newProductWindow.setMenu(null);
+    newProductWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'view/new-product.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+}
+
+ipcMain.on('product:new', (e, newProduct) => {
+    mainWindow.webContents.send('product:new', newProduct);
+    newProductWindow.close();
+    // console.log(newProduct);
+});
+
+const templateMenu = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'New Product',
+                accelerator: 'Ctrl+N',
+                click(){
+                    createNewProductWindow();
+                }
+            },
+            {
+                label: 'Remove all products',
+                submenu: [
+                    {
+                        label: 'option'
+                    }
+                ]
+            },
+            {
+                label: 'Exit',
+                accelerator: process.platform == 'darwin' ? 'command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+    
+]
+
+if(process.env.NODE_ENV !== 'production') {
+    templateMenu.push({
+        label: 'DevTools',
+        submenu: [
+            {
+                label: 'Show/Hide DevTools',
+                accelerator: 'Ctrl+D',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+}
